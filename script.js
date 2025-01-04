bad82dbe982016c2f115e6b15d9dc541b7099dbf
Ankit Kumar <72183626+Ank8873@users.noreply.github.com>
2025-01-04 10:14:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -131,3 +131,13 @@ for(i=1;i<5;i++)
         }
     }
 }
+document.addEventListener("keydown", function(event) {
+
+    if (event.ctrlKey && (event.keyCode === 85 || event.keyCode === 123)) {
+
+      event.preventDefault();
+
+    }
+
+  });
+  document.addEventListener('contextmenu', event => event.preventDefault());
